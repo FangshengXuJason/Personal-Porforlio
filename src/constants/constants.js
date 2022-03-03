027b25eb59e62fd2bedd38aaad5a577f5f361953
FangshengXuaJason <xufangsheng@seattleu.edu>
2022-03-03 15:06:49 -0800

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,69 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'Web-based multiplayer game - Werewolf',
+    description:
+      'One player creates a game room, and the rest join the room. Players can interact in real-time, and the game flows like the in-person game.',
+    image: '/images/werewolf-front-page.PNG',
+    tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://github.com/pmh-3/Werewolf', // source code
+    visit: 'https://www.werewolf.fun/', // live site
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'Poller Bear Poll Service Website',
+    description:
+      'Users can create polls, vote on polls, and view polls’ analytics. ',
+    image: '/images/pollerbear-front-page.PNG',
+    tags: ['ASP .Net', 'C#'],
+    source: 'https://github.com/EdwinKaburu/JOLT', // source code
+    visit: 'https://5110jolt.azurewebsites.net/', // live site
     id: 1,
   },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
-];
+]
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
-];
+  { year: 2018, text: 'Started to Learn Programming' },
+  { year: 2020, text: 'Interned at babelFlex' },
+  { year: 2020.9, text: 'Started MSCS at Seattle University' },
+  { year: 2021.1, text: 'Teamed Up and Build Werewolf' },
+  { year: 2021.3, text: 'Deployed My First Website' },
+]
+
+// export const projects = [
+//   {
+//     title: 'MERN Memories',
+//     description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+//       image: '/images/1.png',
+//       tags: ['Mongo', 'Express', 'React', 'Node'],
+//     source: 'https://google.com',
+//     visit: 'https://google.com',
+//     id: 0,
+//   },
+//   {
+//     title: 'E-Commerce',
+//     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+//     image: '/images/2.png',
+//     tags: ['React', 'JavaScript'],
+//     source: 'https://google.com',
+//     visit: 'https://google.com',
+//     id: 1,
+//   },
+//   {
+//     title: 'WebRTC App',
+//     description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+//       image: '/images/3.jpg',
+//       tags: ['React', 'WebRTC'],
+//     source: 'https://google.com',
+//     visit: 'https://google.com',
+//     id: 2,
+//   },
+//   {
+//     title: 'Unichat',
+//     description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+//     image: '/images/4.jpg',
+//     tags: ['React', 'ChatEngine', 'Firebase'],
+//     source: 'https://google.com',
+//     visit: 'https://google.com',
+//     id: 3,
+//   },
+// ];
