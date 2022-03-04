b449e80ca16103bd2d1d00a62f6f8584e1612e9a
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-03-04 14:35:45 -0800

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,32 +1,53 @@
 export const projects = [
   {
-    title: 'Web-based multiplayer game - Werewolf',
+    title: 'Web-based Werewolf Game',
     description:
-      'One player creates a game room, and the rest join the room. Players can interact in real-time, and the game flows like the in-person game.',
+      'Werewolf is a role-playing multi-player social game. One player creates a game room, and the rest join the room. Players can interact in real-time, and the game flows like the in-person game.',
     image: '/images/werewolf-front-page.PNG',
     tags: ['Mongo', 'Express', 'React', 'Node'],
+    show_source_link: true,
     source: 'https://github.com/pmh-3/Werewolf', // source code
+    show_visit_link: true,
     visit: 'https://www.werewolf.fun/', // live site
     id: 0,
   },
   {
     title: 'Poller Bear Poll Service Website',
     description:
-      'Users can create polls, vote on polls, and view polls’ analytics. ',
+      'Users can create polls, vote on polls, and view polls’ analytics. The website also have a simple log in and registration feature',
     image: '/images/pollerbear-front-page.PNG',
     tags: ['ASP .Net', 'C#'],
+    show_source_link: true,
     source: 'https://github.com/EdwinKaburu/JOLT', // source code
+    show_visit_link: true,
     visit: 'https://5110jolt.azurewebsites.net/', // live site
     id: 1,
+  },
+  {
+    title: 'Pacman Game and Search Algorithm',
+    description:
+      "Automate pacmans'decisoin making to eat power pullet with DFS, BFS and A* Search",
+    image: '/images/pacman.png',
+    tags: ['Search Algorithm', 'AI', 'Python'],
+    show_source_link: true,
+    source: 'https://github.com/FangshengXuJason/Searc-Algo-Pacman-Game', // source code
+    show_visit_link: false,
+    visit: 'https://github.com/FangshengXuJason/Searc-Algo-Pacman-Game', // live site
+    id: 2,
   },
 ]
 
 export const TimeLineData = [
   { year: 2018, text: 'Started to Learn Programming' },
-  { year: 2020, text: 'Interned at babelFlex' },
+  {
+    year: 2020.6,
+    text: 'Graduated with a BS in E.E. from University of Washington',
+  },
+  { year: 2020, text: 'Interned at BabelFlex' },
   { year: 2020.9, text: 'Started MSCS at Seattle University' },
-  { year: 2021.1, text: 'Teamed Up and Build Werewolf' },
-  { year: 2021.3, text: 'Deployed My First Website' },
+  { year: 2021.9, text: 'Started Poller Bear Voting Website' },
+  { year: 2021.1, text: 'Started Werewolf Game Project' },
+  { year: 2022.3, text: 'Deployed My First Website' },
 ]
 
 // export const projects = [
