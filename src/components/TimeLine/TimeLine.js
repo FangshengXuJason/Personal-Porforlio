5cbb22671409c770d07596fda2efa86ddcf484ac
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-08-27 19:31:20 -0700

    update text introduction  in TimeLine.js

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -67,11 +67,10 @@ const Timeline = () => {
     <Section id='about'>
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        👋 I’m a CS grad studnet at Seattle University <br />
-        💞️ I’m interested in Web Dev and Distributed System <br />
-        🌱 I’m currently learning Software Architecture and API Design <br />
-        👀 I’m looking for Part-Time Jobs and Internship/CO-OP Opportunies
-        <br />
+          👋 I’m a CS grad studnet at Seattle University <br />
+          💞️ I’m interested in Web Dev, Mobile Dev and Distributed System <br />
+          🌱 I’m currently learning Firebase and React Native<br />
+          👀 I’m looking full-time software engineering positions<br />
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
